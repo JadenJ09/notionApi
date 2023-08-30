5feae46decdc9f42ee7d3503073d66857a75af56
JadenJ09 <jaden.b.jeong@gmail.com>
2023-08-30 08:57:12 +0200

    second commit

--- main.py
+++ main.py
@@ -38,9 +38,10 @@ def get_pages(num_pages = None):
     #    json.dump(data, f, ensure_ascii=False, indent=4)
 
     results = data["results"]
+    
     while data["has_more"] and get_all:
         payload = {"page_size": page_size, "start_cursor": data["next_cursor"]}
-        url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+        url = f"https://api.notion.com/v1/databases/{database_id}/query"
         response = requests.post(url, json=payload, headers=headers)
         data = response.json()
         results.extend(data["results"])
@@ -52,7 +53,68 @@ pages = get_pages()
 for page in pages:
     page_id = page["id"]
     props = page["properties"]
-    url = props["URL"]["title"][0]["text"]["content"]
-    title = props["Title"]["rich_text"][0]["text"]["content"]
-    published = props["Published"]["date"]["start"]
-    published = datetime.fromisoformat(published)
+    if props["URL"]["title"]:
+        url = props["URL"]["title"][0]["text"]["content"]
+    else:
+        url = None
+    if props["Title"]["rich_text"]:
+        title = props["Title"]["rich_text"][0]["text"]["content"]
+    else:
+        title = None
+    if props["Published"]["date"]:
+        published = props["Published"]["date"]["start"]
+        published = datetime.fromisoformat(published)
+    else:
+        published = None
+
+# %%
+def create_page(data: dict):
+    create_url = "https://api.notion.com/v1/pages"
+
+    payload = {"parent": {"database_id": database_id}, "properties": data}
+
+    res = requests.post(create_url, headers=headers, json=payload)
+    # print(res.status_code)
+    return res
+
+url = "Test URL 2"
+title = "Test Title 2"
+published_date = datetime.now(timezone.utc).isoformat()
+data = {
+    "URL": {"title": [{"text": {"content": url}}]},
+    "Title": {"rich_text": [{"text": {"content": title}}]},
+    "Published": {"date": {"start": published_date, "end": None}}
+}
+
+create_page(data)
+
+# %%
+def update_page(page_id: str, data: dict):
+    url = f"https://api.notion.com/v1/pages/{page_id}"
+
+    payload = {"properties": data}
+
+    res = requests.patch(url, json=payload, headers=headers)
+    return res
+
+page_id = "87f02c10-1f59-4875-82dd-09e297d77827"
+
+title = "Updated Title 2"
+update_data = {"Title": {"rich_text": [{"text": {"content": title}}]}}
+# new_date = datetime(2023, 1, 15).astimezone(timezone.utc).isoformat()
+# update_data = {"Published": {"date": {"start": new_date, "end": None}}}
+
+update_page(page_id, update_data)
+
+# %%
+def delete_page(page_id: str):
+    url = f"https://api.notion.com/v1/pages/{page_id}"
+
+    payload = {"archived": True}
+
+    res = requests.patch(url, json=payload, headers=headers)
+    return res
+
+page_id = "87f02c10-1f59-4875-82dd-09e297d77827"
+
+delete_page(page_id)
